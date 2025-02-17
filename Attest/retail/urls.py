38cd6a15c5b514242871c3d0b371d92ae5b54fb3
SergeyUpgrade <sergey.vasilev.98@mail.ru>
2025-02-17 22:24:34 +0300

    Delete Attest directory

--- Attest/retail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.permissions import AllowAny
-
-from . import views
-
-urlpatterns = [
-    path('nodes/', views.NetworkNodeListCreate.as_view(permission_classes=(AllowAny,)), name='node-list'),
-    path('nodes/<int:pk>/', views.NetworkNodeRetrieveUpdateDestroy.as_view(permission_classes=(AllowAny,)), name='node-detail'),
-]
